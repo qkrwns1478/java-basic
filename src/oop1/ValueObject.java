804ff9fd8fc426ce6e0f30ded59944bd05ca6230
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-13 19:04:42 +0900

    25. 클래스와 메서드

---
+++ src/oop1/ValueObject.java
@@ -0,0 +1,12 @@
+package oop1;
+
+public class ValueObject {
+    int value;
+
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value = " + value);
+    }
+
+    // 객체 지향 프로그래밍: 속성과 기능이 합쳐져 있다.
+}
