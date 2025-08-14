92c7c4a6b7efff13bf8fbb53f3bf67654b016d80
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-14 16:17:02 +0900

    30. this

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,4 +4,10 @@ public class MemberInit {
     String name;
     int age;
     int score;
+
+    void initMember(String name, int age, int score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
 }
