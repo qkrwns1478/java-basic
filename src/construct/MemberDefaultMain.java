68b583e2f5f0680925c4e250e1a87871bea6794b
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-14 16:46:52 +0900

    32. 기본 생성자

---
+++ src/construct/MemberDefaultMain.java
@@ -0,0 +1,7 @@
+package construct;
+
+public class MemberDefaultMain {
+    public static void main(String[] args) {
+        MemberDefault member = new MemberDefault(); // 기본 생성자가 자동으로 만들어짐
+    }
+}
