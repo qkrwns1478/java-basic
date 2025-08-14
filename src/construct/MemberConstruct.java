34392da6410fac40483188c2762a5aedd9e12d97
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-14 16:32:51 +0900

    31. 생성자 - 도입

---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int score;
+
+    // 생성자의 이름은 클래스명과 같고, 반환 타입이 없어야 한다.
+    MemberConstruct(String name, int age, int score) {
+        System.out.println("생성자 호출 name=" + name + ",age=" + age + ",score=" + score);
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+}
