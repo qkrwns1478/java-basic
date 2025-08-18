0b3ecf15ca877a7e43ab767efe37a4cae10b6407
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-18 18:00:41 +0900

    42. 접근 제어자 이해2

--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -13,7 +13,7 @@ public class SpeakerMain {
 
         // 필드에 직접 접근
         System.out.println("volume 필드 직접 접근 수정");
-        speaker.volume = 200;
-        speaker.showVolume(); // 현재 음량: 200 → 스피커 터짐🤯
+        // speaker.volume = 200; // private 접근 오류
+        speaker.showVolume();
     }
 }
