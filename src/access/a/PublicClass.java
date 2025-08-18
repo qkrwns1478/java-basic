80ea278c21bc3c8d7a92b6920404c91edc9bfd03
qkrwns1478 <qkrwns1478@gmail.com>
2025-08-18 18:25:17 +0900

    45. 접근 제어자 사용 - 클래스 레벨

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,18 @@
+package access.a;
+
+/* public 클래스명은 반드시 파일 이름과 같아야 한다. */
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass1 defaultClass1 = new DefaultClass1();
+        DefaultClass2 defaultClass2 = new DefaultClass2();
+    }
+}
+
+class DefaultClass1 {
+
+}
+
+class DefaultClass2 {
+
+}
